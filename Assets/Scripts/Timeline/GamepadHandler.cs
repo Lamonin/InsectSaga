fb898a93c5618581b5f5fa5d06d107bdaca64e93
Lamonin <maze.zte@gmail.com>
2022-04-17 17:39:26 +1000

    Gamepad handler fix

--- Assets/Scripts/Timeline/GamepadHandler.cs
+++ Assets/Scripts/Timeline/GamepadHandler.cs
@@ -9,26 +9,33 @@ public class GamepadHandler:MonoBehaviour
     private float _power;
     private float _duration;
     private Coroutine _repeatVibrationRoutine;
+    private bool isStillVibration;
 
     public void Vibrate()
     {
+        if (Gamepad.current == null) return;
         Gamepad.current.SetMotorSpeeds(_power, _power);
+        isStillVibration = true;
         _repeatVibrationRoutine = StartCoroutine(RepeatingVibration());
     }
 
     public void StopVibrate()
     {
-        Gamepad.current.SetMotorSpeeds(_power, _power);
+        if (Gamepad.current == null) return;
+        Gamepad.current.SetMotorSpeeds(0, 0);
+        isStillVibration = false;
         if (_repeatVibrationRoutine != null)
             StopCoroutine(RepeatingVibration());
     }
 
     private IEnumerator RepeatingVibration()
     {
-        while (true)
+        while (isStillVibration)
         {
+            if (Gamepad.current == null) yield break;
+            Gamepad.current.SetMotorSpeeds(0, 0);
             Gamepad.current.SetMotorSpeeds(_power, _power);
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(2f);
         }
     }
 
