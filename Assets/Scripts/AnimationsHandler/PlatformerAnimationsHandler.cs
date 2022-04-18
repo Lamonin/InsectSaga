090ef15c97c354d3edd30a6d7cb9b41cbc7b95e6
Lamonin <maze.zte@gmail.com>
2022-04-19 00:15:34 +1000

    Fix walk animation

--- Assets/Scripts/AnimationsHandler/PlatformerAnimationsHandler.cs
+++ Assets/Scripts/AnimationsHandler/PlatformerAnimationsHandler.cs
@@ -11,7 +11,6 @@ namespace AnimationsHandler
 
         private readonly int IDLE = Animator.StringToHash("idle");
         private readonly int WALK = Animator.StringToHash("walk");
-        private readonly int RUN = Animator.StringToHash("run");
         private readonly int JUMP = Animator.StringToHash("jump");
 
         void Start()
@@ -25,7 +24,7 @@ namespace AnimationsHandler
             {
                 if (chController.moveDir != 0 && Mathf.Abs(chController.GetRigidBody2D.velocity.x) > 0.02f)
                 {
-                    _frameAnimator.ChangeAnimation(Mathf.Abs(chController.moveDir) <= chController.stickOffsetBeforeRun ? WALK : RUN);
+                    _frameAnimator.ChangeAnimation(WALK);
                 }
                 else
                 {
