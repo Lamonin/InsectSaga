dcfc5602e370802de735be60dda88a27f2791b03
Maxim <maze.zte@gmail.com>
2022-05-14 11:53:35 +1000

    Add script to Load Main Menu on start

---
+++ Assets/Scripts/Editor/LoadDefaultSceneOnPlayEditor.cs
@@ -0,0 +1,26 @@
+#define ENABLED
+
+#if ENABLED
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class LoadDefaultSceneOnPlayEditor
+{
+    static LoadDefaultSceneOnPlayEditor(){
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state){
+        if (state == PlayModeStateChange.ExitingEditMode) {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo ();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode) {
+            EditorSceneManager.LoadScene (0);
+        }
+    }
+}
+#endif
+#endif
